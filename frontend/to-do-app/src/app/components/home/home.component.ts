cc74ce9fefbf22e23ad7f2213d40f115c833664a
Avani Prajapati <darshal.dalal.ordex@gmail.com>
2025-04-21 13:38:20 +0530

    complete todo part

--- frontend/to-do-app/src/app/components/home/home.component.ts
+++ frontend/to-do-app/src/app/components/home/home.component.ts
@@ -12,6 +12,8 @@ import { FormsModule } from '@angular/forms';
 export class HomeComponent implements OnInit {
   todos: Todo[] = [];
   newTitle = '';
+  todoIdToDelete: string | null = null;
+  bgColors: string[] = ['#99aa38', '#fad2e1', '#9882ac', '#89c2d9', '#d4a373'];
   constructor(private todoService: TodoService) {}
 
   ngOnInit() {
@@ -32,11 +34,35 @@ export class HomeComponent implements OnInit {
   }
 
   toggleCompleted(todo: Todo) {
-    todo.completed = !todo.completed;
     this.todoService.updateTodo(todo).subscribe();
   }
 
-  deleteOneTodo(id: string) {
-    this.todoService.deleteTodo(id).subscribe(() => this.fetchTodos());
+  // deleteOneTodo(id: string) {
+  //   this.todoService.deleteTodo(id).subscribe(() => this.fetchTodos()); 
+  // }
+  setTodoToDelete(id: string) {
+    this.todoIdToDelete = id;
+    console.log(this.todoIdToDelete)
   }
+  // confirmDelete(){
+  //    if (this.todoIdToDelete) {
+  //   this.deleteOneTodo(this.todoIdToDelete);
+  //   this.todoIdToDelete = null;
+  // }
+  // }
+
+  confirmDelete(): void {
+    if (!this.todoIdToDelete) return;
+  
+    this.todoService.deleteTodo(this.todoIdToDelete).subscribe({
+      next: () => {
+        this.fetchTodos();
+        this.todoIdToDelete = null;
+      },
+      error: (err) => {
+        console.error('Failed to delete todo:', err);
+      }
+    });
+  }
+  
 }
